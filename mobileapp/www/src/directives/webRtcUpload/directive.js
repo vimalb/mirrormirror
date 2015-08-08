783e7ee354a152932583c5b35c478851699b930f
Puppy Copter <hackathon@puppycopter.com>
2015-08-08 00:40:54 -0700

    webrtc recording

--- mobileapp/www/src/directives/webRtcUpload/directive.js
+++ mobileapp/www/src/directives/webRtcUpload/directive.js
@@ -83,18 +83,132 @@ angular.module(MODULE_NAME, [])
 
             $scope.startRecording = function() {
                 $scope.recordingState = true;
-                $scope.recordingId = getRecordingId();
 
                 console.log(CLIENT_SETTINGS.WEBRTC_SERVER_URL);
     
+                var videoOutput = $(element).find('.uploadVideoOutput')[0];
 
-                $scope.onRecordingStart({"recordingId": $scope.recordingId});
-                $scope.stopRecording = function(event){
-                  console.log("Stopping recording");
-                  $scope.recordingState = false;
-                  $scope.stopRecording = function(){};
+                var thumbnailCanvas = $(element).find('.uploadVideoThumbnail')[0];
+                thumbnailCanvas.width = videoOutput.width;
+                thumbnailCanvas.height = videoOutput.height;
+                var thumbnailCtx = thumbnailCanvas.getContext("2d");
+                $scope.getScreenshot = function() {
+                  thumbnailCtx.drawImage(videoOutput, 0, 0, videoOutput.width, videoOutput.height);
+                  return thumbnailCanvas.toDataURL("image/png");
+                }
+
+
+                $scope.recordingId = getRecordingId();
+                var fileUri = 'file://'+SERVER_SETTINGS.RECORDING_ROOT+'/'+$scope.recordingId+'.webm'
+                console.log("Recording to", fileUri);
+
+                var args = {
+                  ws_uri: CLIENT_SETTINGS.WEBRTC_SERVER_URL+'?cachebuster='+((new Date()).getTime().toString()),
+                  file_uri: fileUri,
+                  ice_servers: undefined
                 };
-                  
+
+                var options = {
+                  remoteVideo: videoOutput
+                };
+
+                if (args.ice_servers) {
+                  console.log("Use ICE servers: " + args.ice_servers);
+                  options.configuration = {
+                    iceServers : JSON.parse(args.ice_servers)
+                  };
+                } else {
+                  console.log("Use freeice")
+                }
+
+                var setIceCandidateCallbacks = function(webRtcPeer, webRtcEp, onerror)
+                {
+                  webRtcPeer.on('icecandidate', function(candidate) {
+                    console.log("Local candidate:",candidate);
+                    candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
+                    webRtcEp.addIceCandidate(candidate, onerror)
+                  });
+
+                  webRtcEp.on('OnIceCandidate', function(event) {
+                    var candidate = event.candidate;
+                    console.log("Remote candidate:",candidate);
+                    webRtcPeer.addIceCandidate(candidate, onerror);
+                  });
+                }
+
+                var onError = function(){};
+
+
+                var webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(error)
+                {
+                  if(error) return onError(error)
+                  this.generateOffer(onOffer)
+                });
+
+
+                function onOffer(error, offer) {
+                    if (error) return onError(error);
+
+                    console.log("Offer...");
+
+                    kurentoClient(args.ws_uri, function(error, client) {
+                      if (error) return onError(error);
+
+                      client.create('MediaPipeline', function(error, pipeline) {
+                        if (error) return onError(error);
+
+
+                        var elements =
+                        [
+                          {type: 'RecorderEndpoint', params: {uri : args.file_uri}},
+                          {type: 'WebRtcEndpoint', params: {}}
+                        ]
+
+                        pipeline.create(elements, function(error, elements){
+                          if (error) return onError(error);
+
+                          var recorder = elements[0]
+                          var webRtc   = elements[1]
+
+                          setIceCandidateCallbacks(webRtcPeer, webRtc, onError)
+
+                          webRtc.processOffer(offer, function(error, answer) {
+                            if (error) return onError(error);
+
+                            console.log("offer");
+
+                            webRtc.gatherCandidates(onError);
+                            webRtcPeer.processAnswer(answer);
+                          });
+
+                          client.connect(webRtc, webRtc, recorder, function(error) {
+                            if (error) return onError(error);
+
+                            console.log("Connected");
+
+                            recorder.record(function(error) {
+                              if (error) return onError(error);
+
+                              console.log("record");
+
+                              $scope.onRecordingStart({"recordingId": $scope.recordingId});
+                              $scope.stopRecording = function(event){
+                                console.log("Stopping recording");
+                                recorder.stop();
+                                pipeline.release();
+                                webRtcPeer.dispose();
+                                videoOutput.src = "";
+                                $scope.recordingState = false;
+                                $scope.stopRecording = function(){};
+                              };
+
+                            });
+                          });
+                        });
+                      });
+                    });
+
+                }
             }
 
 
