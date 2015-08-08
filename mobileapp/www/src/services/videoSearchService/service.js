00588b269d424eba9e3a1c631d4177b222fb53d0
Puppy Copter <hackathon@puppycopter.com>
2015-08-07 22:24:58 -0700

    updated video search service

--- mobileapp/www/src/services/videoSearchService/service.js
+++ mobileapp/www/src/services/videoSearchService/service.js
@@ -13,53 +13,42 @@ document.APP_MODULES.push(MODULE_NAME);
 
 console.log(MODULE_NAME, "Registering service", SERVICE_NAME);
 angular.module(MODULE_NAME, [])
-    .factory(SERVICE_NAME, function() {
+    .factory(SERVICE_NAME, function($q, CLIENT_SETTINGS, $http) {
       console.log("Instantiating service", SERVICE_NAME);
 
-      // Some fake testing data
-      var chats = [{
-        id: 0,
-        name: 'Ben Sparrow',
-        lastText: 'You on your way?',
-        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-      }, {
-        id: 1,
-        name: 'Max Lynx',
-        lastText: 'Hey, it\'s me',
-        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-      }, {
-        id: 2,
-        name: 'Adam Bradleyson',
-        lastText: 'I should buy a boat',
-        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-      }, {
-        id: 3,
-        name: 'Perry Governor',
-        lastText: 'Look at my mukluks!',
-        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-      }, {
-        id: 4,
-        name: 'Mike Harrington',
-        lastText: 'This is wicked good ice cream.',
-        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-      }];
-
       return {
-        all: function() {
-          return chats;
+        search: function(searchRequest) {
+          var deferred = $q.defer();
+          var url = CLIENT_SETTINGS.SERVER_URL + '/api/recordings';
+          $http.get(url).then(function(resp) {
+            var videos = _.filter(resp.data, function(video) {
+              return _.includes(['live', 'finished'], video.state);
+            });
+            deferred.resolve(videos);
+          });
+          return deferred.promise;
         },
-        remove: function(chat) {
-          chats.splice(chats.indexOf(chat), 1);
+        get: function(videoId) {
+          var deferred = $q.defer();
+          var url = CLIENT_SETTINGS.SERVER_URL + '/api/recordings/' + videoId;
+          $http.get(url).then(function(resp) {
+            deferred.resolve(resp.data);
+          });
+          return deferred.promise;
         },
-        get: function(chatId) {
-          for (var i = 0; i < chats.length; i++) {
-            if (chats[i].id === parseInt(chatId)) {
-              return chats[i];
-            }
+        update: function(videoInfo) {
+          if(videoInfo.recordingId) {
+            var deferred = $q.defer();
+            var url = CLIENT_SETTINGS.SERVER_URL + '/api/recordings/' + videoInfo.recordingId;
+            $http.post(url, JSON.stringify(videoInfo)).then(function(resp) {
+              console.log("Updated recording", resp.data);
+              deferred.resolve(resp.data);
+            });
+            return deferred.promise;
           }
-          return null;
         }
       };
+
     });
   
   
